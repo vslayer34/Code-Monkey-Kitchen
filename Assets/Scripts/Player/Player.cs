2c3f71218310bd71f40956973f3e93a672d98270
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-07-07 02:48:16 +0300

    Added improvements to the player controller

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,16 +13,78 @@ public class Player : MonoBehaviour
 
     private bool _isWalking;
 
+    private const float PLAYER_RADIUS = 0.7f;
+    private const float PLAyER_HEIGHT = 2.0f;
+
 
 
     // Game Loop Methods---------------------------------------------------------------------------
     private void Update()
     {
         Vector2 inputVector = GameInput.InputVectorNormalized;
-
         Vector3 moveDirection = new Vector3(inputVector.x, 0.0f, inputVector.y);
 
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(
+            transform.position, 
+            transform.position + Vector3.up * PLAyER_HEIGHT, 
+            PLAYER_RADIUS, 
+            moveDirection, 
+            moveDistance
+        );
+
+        if (!canMove)
+        {
+            // Cannot move in that direction
+            // Attempt moving in the x drection
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0.0f, 0.0f);
+
+            canMove = !Physics.CapsuleCast(
+                transform.position,
+                transform.position + Vector3.up * PLAyER_HEIGHT,
+                PLAYER_RADIUS,
+                moveDirectionX,
+                moveDistance
+            );
+
+            if (canMove)
+            {
+                // move along the x axis
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                // can't move in the x axis
+                // Attempt moving in the z axis
+                Vector3 moveDirectionZ = new Vector3(0.0f, 0.0f, moveDirection.z);
+
+                canMove = !Physics.CapsuleCast(
+                    transform.position,
+                    transform.position + Vector3.up * PLAyER_HEIGHT,
+                    PLAYER_RADIUS,
+                    moveDirectionZ,
+                    moveDistance
+                );
+
+                if (canMove)
+                {
+                    // Move along the z axis
+                    moveDirection = moveDirectionZ;
+                }
+                else
+                {
+                    // Can't move at all
+                }
+            }
+
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
+
+        // For animations
         _isWalking = moveDirection != Vector3.zero;
 
         float rotationSpeed = 10.0f;
