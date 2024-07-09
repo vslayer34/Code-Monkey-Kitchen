516d36c0bf066227249faabd4d9e6cbab9117ce4
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-07-09 08:40:55 +0300

    Moved the kitchen object visual and game object parent status to the new counter

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -10,14 +10,66 @@ public class ClearCounter : MonoBehaviour
     [SerializeField, Tooltip("Reference to the tomato")]
     private SO_KitchenObject _kitchenObject;
 
+    private KitchenObject _kitchenObjOnCounter;
+
+    // Testing
+    [SerializeField]
+    private bool _testMode;
+
+    [SerializeField]
+    private ClearCounter _testCounter;
+
 
 
     // Member Methods------------------------------------------------------------------------------
+
+    private void Update()
+    {
+        if (_testMode && Input.GetKeyDown(KeyCode.T))
+        {
+            if (_kitchenObjOnCounter != null)
+            {
+                _kitchenObjOnCounter.SetParentCounter(_testCounter);
+            }
+        }
+    }
+
     public void Interact()
     {
-        var newKitchenObject = Instantiate(_kitchenObject.Prefab, _topPlacementPoint);
-        newKitchenObject.localPosition = Vector3.zero;
+        if (_kitchenObjOnCounter == null)
+        {
+            var newKitchenObject = Instantiate(_kitchenObject.Prefab, _topPlacementPoint);
 
-        Debug.Log($"Spawned {newKitchenObject.GetComponent<KitchenObject>().KitchenObj.name}");
+            newKitchenObject.GetComponent<KitchenObject>().SetParentCounter(this);
+        }
+        else
+        {
+            Debug.Log(_kitchenObjOnCounter.GetParentCounter());
+        }
+    }
+
+    public Transform GetKitchenObjectPlacementPoint()
+    {
+        return _topPlacementPoint;
+    }
+
+    public bool IsKitchenObjectOnCounter() => _kitchenObjOnCounter != null;
+
+    // Getters & Setters---------------------------------------------------------------------------
+
+    public KitchenObject KitchenObjectOnCounter
+    { 
+        get => _kitchenObjOnCounter;
+        set
+        {
+            if (!IsKitchenObjectOnCounter() || value == null)
+            {
+                _kitchenObjOnCounter = value;
+            }
+            else
+            {
+                Debug.LogError("There is an object already on the counter");
+            }
+        }
     }
 }
--- Assets/Scripts/Kitchen Objects/KitchenObject.cs
+++ Assets/Scripts/Kitchen Objects/KitchenObject.cs
@@ -6,4 +6,28 @@ public class KitchenObject : MonoBehaviour
 {
     [field: SerializeField, Tooltip("Reference to the this object stats")]
     public SO_KitchenObject KitchenObj { get; private set; }
+
+
+    private ClearCounter _parentCounter;
+
+
+
+    // Member Methods------------------------------------------------------------------------------
+
+    public void SetParentCounter(ClearCounter clearCounter)
+    {
+        // Remove the old parent
+        if (_parentCounter != null)
+        {
+            _parentCounter.KitchenObjectOnCounter = null;
+        }
+
+        // Make the new counter the new parent
+        _parentCounter = clearCounter;
+        _parentCounter.KitchenObjectOnCounter = this;
+
+        transform.parent = _parentCounter.GetKitchenObjectPlacementPoint();
+        transform.localPosition = Vector3.zero;
+    }
+    public ClearCounter GetParentCounter() => _parentCounter;
 }
