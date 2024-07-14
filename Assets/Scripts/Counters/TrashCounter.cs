c7cfbab4c4fd5f721e6a7a0577ae08f869544b8d
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-07-14 14:44:44 +0300

    Added trash counters

---
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCounter : BaseCounter
+{
+    public override void Interact(Player player) => player.KitchenObjectOnCounter?.DestroySelf();
+}
